01200762277be40241df2d68a60e84b14671c43c
Pinaki Upadhyay <pinaki.u.15@gmail.com>
2023-10-04 21:49:48 -0400

    Created donations table

--- kindnesscaptured/donations/models.py
+++ kindnesscaptured/donations/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import EmployeeUser, DonaterUser
 
-# Create your models here.
+class Donation(models.Model):
+    USE_CHOICES = (
+        ('new', 'New'),
+        ('lightly used', 'Lightly Used'),
+        ('old', 'Old'),
+    )
+
+    donater = models.ForeignKey(DonaterUser, on_delete=models.CASCADE)
+    front_picture = models.ImageField(upload_to='donations/pictures')
+    back_picture = models.ImageField(upload_to='donations/pictures')
+    wear_tear = models.BooleanField(default=False)
+    stain_odor_damage = models.BooleanField(default=False)
+    use = models.CharField(max_length=50, choices=USE_CHOICES)
+    ai_score = models.JSONField()
+    approval = models.BooleanField(default=False)
+
